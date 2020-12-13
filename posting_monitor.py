c8c751deb2d6def27c498e7c0ebcffcb8cde94c7
joy <joy@joyui-MacBookPro.local>
2020-12-14 01:11:32 +0900

    modify posting_monitor.py

--- posting_monitor.py
+++ posting_monitor.py
@@ -7,15 +7,18 @@ import os
 from filecmp import cmp
 import difflib
 
+MONITORING_TIME_INTERVAL = 10
+
 url1 = 'https://docs.google.com/document/d/1i9j_XGRqFNHX-nu3ehOQ3RgUpLCHNtctCXrXTNKMGVw/edit'
 url2 = 'http://acebedmall.co.kr/front/search/categorySearch.do?searchYn=N&ctgNo=2'
-url = 'https://www.skku.edu/skku/campus/skk_comm/notice01.do'
-urls = [url2]
+url = 'https://blog.naver.com/harim9355'
+urls = [url]
 
 driver = webdriver.Chrome()
 
 
-def get_html(url):
+def _get_html_from_url(url):
+    """ get html from url """
     driver.get(url)
     page_source = driver.page_source
     html = str(BeautifulSoup(page_source, "html.parser"))
@@ -23,67 +26,80 @@ def get_html(url):
     return html
 
 
-
-for index, url in enumerate(urls, 1):
-    html = get_html(url)
-
-    # save html to text
-    with open(f'{index}.txt', 'w', -1, 'utf-8') as f:
+def _save_html_as_file(html, file_name):
+    """ save html as file """
+    with open(file_name, 'w', -1, 'utf-8') as f:
         f.write(html)
 
 
+def _get_link_from_html(html):
+    """ get link form html """
+    links = []
+    soup = BeautifulSoup(html, "html.parser")
+    anchors = soup.find_all('a', href=True)
+
+    for anchor in anchors:
+        links.append(anchor['href'])
+    
+    return links
+
+
+def _get_link_from_diff_file(file_name):
+    """ get link from diff file """
+    links = []
+    with open(file_name, 'r', -1, 'utf-8') as f:
+        lines = f.readlines()
+        for line in lines:
+            if line[0] == '>':
+                links.extend(_get_link_from_html(line))
+
+    return links
+
+
+
+# save urls' html as file
+for index, url in enumerate(urls, 1):
+    file_name = f'{index}.txt'
+
+    html = _get_html_from_url(url)
+    _save_html_as_file(html, file_name)
+
+
+# monitor web
 while True:
     for index, url in enumerate(urls, 1):
-        new_html = get_html(url)
         file_name = f'{index}.txt'
         new_file_name = f'new_{index}.txt'
+        diff_file_name = f'diff_{index}.txt'
 
-        with open(new_file_name, 'w', -1, 'utf-8') as f:
-            f.write(new_html)
+        new_html = _get_html_from_url(url)
+        _save_html_as_file(new_html, new_file_name)
+        
+        # make diff file
+        os.system(f'diff {file_name} {new_file_name} > {diff_file_name}')
 
-        if cmp(file_name, new_file_name) is False:
-            # os.system(f'diff {file_name} {new_file_name} > diff_{index}.txt')
-            diff = difflib.Differ()
-            a = "aaa\n"
-            b = "bbb"
-            print(diff.compare(a, b))
+        links = _get_link_from_diff_file(diff_file_name)
+
+
+        if links:
             print('Posting Uploaded !!')
-            
-
-        time.sleep(10)
-
-
-"""
-import telegram
-bot = telegram.Bot(token='123412345:ABCDEFgHiJKLmnopqr-0StUvwaBcDef0HI4jk')
-chat_id = bot.getUpdates()[-1].message.chat.id
-
-# 파일의 위치
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-
-
-
-
-while True:
-
-    html = driver.page_source
-    soup = BeautifulSoup(html, "html.parser")
-
-    results = soup.select(".isv-r")
-
-
-    with open(os.path.join(BASE_DIR, 'latest.txt'), 'r+') as f_read:
-        before = f_read.readline()
-        if before != latest:
-            bot.sendMessage(chat_id=chat_id, text='새 글이 올라왔어요!')
         else:
-            bot.sendMessage(chat_id=chat_id, text='새 글이 없어요 ㅠㅠ')
-        f_read.close()
+            print('No Posting Uploaded !!')
 
-    with open(os.path.join(BASE_DIR, 'latest.txt'), 'w+') as f_write:
-        f_write.write(latest)
-        f_write.close()
 
-    time.sleep(60) # 60초(1분)을 쉬어줍니다.
-"""
+        # send message to user
+        for i in range(len(links)):
+            if links[i][0] == '/':
+                links[i] = url + links[i]
+            # send_message(index, i)
+
+
+        # update file if html is changed
+        if cmp(file_name, new_file_name) is False:
+            os.system(f'rm {file_name}')
+            os.system(f'mv {new_file_name} {file_name}')
+
+        
+        # sleep for monitoring time interval
+        time.sleep(MONITORING_TIME_INTERVAL)
+
--- test.py
+++ test.py
@@ -1,8 +1,65 @@
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+import os
+from filecmp import cmp
 import difflib
 
+url1 = 'https://docs.google.com/document/d/1i9j_XGRqFNHX-nu3ehOQ3RgUpLCHNtctCXrXTNKMGVw/edit'
+url2 = 'http://acebedmall.co.kr/front/search/categorySearch.do?searchYn=N&ctgNo=2'
+url = 'https://blog.naver.com/harim9355'
+urls = [url]
 
-diff = difflib.Differ()
-a = "aaa\n"
-b = "bbb"
-print(diff.compare(a, b))
+driver = webdriver.Chrome()
+
+
+def _get_html_from_url(url):
+    """ get html from url """
+    driver.get(url)
+    page_source = driver.page_source
+    html = str(BeautifulSoup(page_source, "html.parser"))
+
+    return html
+
+
+def _save_html_as_file(html, file_name):
+    """ save html as file """
+    with open(file_name, 'w', -1, 'utf-8') as f:
+        f.write(html)
+
+
+def _get_link_from_html(html):
+    """ get link form html """
+    links = []
+    soup = BeautifulSoup(html, "html.parser")
+    anchors = soup.find_all('a', href=True)
+
+    for anchor in anchors:
+        links.append(anchor['href'])
+    
+    return links
+
+
+def _get_link_from_diff_file(file_name):
+    """ get link from diff file """
+    links = []
+    with open(file_name, 'r', -1, 'utf-8') as f:
+        lines = f.readlines()
+        for line in lines:
+            if line[0] == '>':
+                links.extend(_get_link_from_html(line))
+
+    return links
+
+
+
+# save urls' html as file
+for index, url in enumerate(urls, 1):
+    file_name = 'naver2.txt'
+
+    html = _get_html_from_url(url)
+    _save_html_as_file(html, file_name)
+
 
